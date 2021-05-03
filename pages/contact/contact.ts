b806bf680d7cdca33ea72dfcadd2e0fef7ad1d7d
EinsAlbertoJose <jose.alberto080159@my.lsgh.edu.ph>
2021-05-03 08:42:49 +0800

    2 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -10,7 +10,6 @@ export class ContactPage {
   CombHeight: number;
   HeightFt: number;
   HeightIn: number;
-  FactHeight: number;
   WRob: number;
   WMil: number;
   WHam: number;
@@ -20,45 +19,54 @@ export class ContactPage {
   constructor(public navCtrl: NavController) {}
 
   Compute(){
+    this.CombHeight = ((this.HeightFt*12) + this.HeightIn) - 60;
     
-    this.CombHeight = (this.HeightFt*12) + this.HeightIn;
-    this.WRob =parseFloat(this.WRob.toFixed(2));
-    this.WMil =parseFloat(this.WMil.toFixed(2));
-    this.WHam =parseFloat(this.WHam.toFixed(2));
-    this.WDev =parseFloat(this.WDev.toFixed(2));
-    
-      if (this.gender = "m") {
+      if (this.gender == "m") {
         
-        if (this.CombHeight <= 60){
+        if (this.CombHeight <= 0){
           this.WRob = 52.00
           this.WMil = 56.20
           this.WHam = 48.00
-          this.WDev = 50.00
+          this.WDev = 50.00;
 
         } else{
-          this.FactHeight = this.CombHeight - 60
           
-          this.WRob = 52.00 + (this.FactHeight*1.90)
-          this.WMil = 56.20 + (this.FactHeight*1.41)
-          this.WHam = 48.00 + (this.FactHeight*2.70)
-          this.WDev = 50.00 + (this.FactHeight*2.30)
+          this.WRob = 52.00 + (this.CombHeight*1.90)
+            this.WRob =parseFloat(this.WRob.toFixed(2));
+            
+          this.WMil = 56.20 + (this.CombHeight*1.41)
+            this.WMil =parseFloat(this.WMil.toFixed(2));
+
+          this.WHam = 48.00 + (this.CombHeight*2.70)
+            this.WHam =parseFloat(this.WHam.toFixed(2));
+
+          this.WDev = 50.00 + (this.CombHeight*2.30)
+            this.WDev =parseFloat(this.WDev.toFixed(2));
+
         }
 
-      } else if (this.gender= "f") {
+      } else if (this.gender == "f") {
         
-        if (this.CombHeight <= 60){
+        if (this.CombHeight <= 0){
           this.WRob = 49.00
           this.WMil = 53.10
           this.WHam = 45.50
-          this.WDev = 45.50
+          this.WDev = 45.50;
 
         } else{
-          this.FactHeight = this.CombHeight - 60
           
-          this.WRob = 49.00 + (this.FactHeight*1.70)
-          this.WMil = 53.10 + (this.FactHeight*1.36)
-          this.WHam = 45.50 + (this.FactHeight*2.20)
-          this.WDev = 45.50 + (this.FactHeight*2.30)
+          this.WRob = 49.00 + (this.CombHeight*1.70);
+            this.WRob =parseFloat(this.WRob.toFixed(2));
+
+          this.WMil = 53.10 + (this.CombHeight*1.36);
+            this.WMil =parseFloat(this.WMil.toFixed(2));
+
+          this.WHam = 45.50 + (this.CombHeight*2.20);
+            this.WHam =parseFloat(this.WHam.toFixed(2));
+
+          this.WDev = 45.50 + (this.CombHeight*2.30);
+            this.WDev =parseFloat(this.WDev.toFixed(2));
+
         }
 
       }else {
